a07e8bd437469f2b8c671267387d7caa88d39a8d
Shiro-LK <shirosaki94@gmail.com>
2018-06-06 10:18:14 +0200

    'update'

--- research/object_detection/data_decoders/tf_example_decoder.py
+++ research/object_detection/data_decoders/tf_example_decoder.py
@@ -187,6 +187,7 @@ class TfExampleDecoder(data_decoder.DataDecoder):
           format_key='image/format',
           channels=3,
           dct_method=dct_method)
+      exit()
     else:
 #      image = slim_example_decoder.Image(
 #          image_key='image/encoded', format_key='image/format', channels=3)
